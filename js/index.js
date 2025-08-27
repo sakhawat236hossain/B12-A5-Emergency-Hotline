d6a16f87900cc58f0fc4682347b73ec114bebda6
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-08-28 00:35:09 +0600

    call button done

---
+++ js/index.js
@@ -0,0 +1,73 @@
+
+// funtion getElementById
+function getInnerText(id) {
+  const element = document.getElementById(id);
+  const elementValue = element.innerText;
+  const elementValueNumber = parseInt(elementValue);
+  return elementValueNumber;
+}
+
+// heard-btns section
+
+let heardNumber = getInnerText("heard-amount");
+
+const heardBtns = document.getElementsByClassName("heart-btn");
+
+//  click heard heardBtns
+
+for (let i = 0; i < heardBtns.length; i++) {
+  heardBtns[i].addEventListener("click", function () {
+    let totleHeardNumber = heardNumber++;
+    document.getElementById("heard-amount").innerText = totleHeardNumber;
+  });
+}
+
+// call button sectio
+let coinNumber = 100;
+const coinElement = document.getElementById("coin-amount");
+coinElement.innerText = coinNumber;
+
+// const callBtns = document.getElementsByClassName("call-btn");
+// const cardTitle = document.getElementsByClassName('card-title')
+// const cardTitleInnerText = cardTitle.innerText
+// const cardNumber = document.getElementsByClassName('card-number')
+// // const cardNumberInnerText =cardNumber.inn
+// console.log(cardNumber,cardTitle);
+const callBtns = document.getElementsByClassName("call-btn");
+for (const callBtn of callBtns) {
+  callBtn.addEventListener("click", function () {
+ let cardParent = callBtn.parentNode.parentNode;
+
+    const cardTitle = cardParent.querySelector(".card-title").innerText
+    const cardNumber = cardParent.querySelector(".card-number").innerText;
+    if (coinNumber >= 20) {
+      alert(`${cardTitle}\n${cardNumber}`);
+      coinNumber -= 20;
+      coinElement.innerText = coinNumber;
+    } else {
+      alert(
+        "You don't have enough coins. You need at least 20 coins to make a call."
+      );
+    }
+    const historyContainar = document.getElementById("add-history")
+    const newHeistory = document.createElement("div")
+     const newdate = new Date();
+     const time = newdate.toLocaleString();
+
+    newHeistory.innerHTML = `
+      <div class="mt-4 bg-amber-50">
+             <div class="flex justify-between items-center">
+             <div>
+              <h1 class="font-bold">${cardTitle }</h1>
+              <p class="font-bold">${cardNumber}</p>
+             </div>
+              <p>${time}</p>
+            </div>
+         </div>
+    `;
+    historyContainar.append(newHeistory)
+    
+  });
+
+
+}
